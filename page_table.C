52f1095a3d7a1a6f6aa45cf37010e9ee957b042b
arjunjm <arjunjm@gmail.com>
2014-02-05 03:49:11 -0600

    Added some of the function definitions in the page table class

--- page_table.C
+++ page_table.C
@@ -2,7 +2,39 @@
 
 PageTable::PageTable()
 {
-    unsigned long *page_directory = (unsigned long*) 0X9C000;
+    /* Any 4k-aligned address can be used for the page_directory */
+    page_directory = (unsigned long*) 0X9C000;
+
+    /* The page table follows the page_directory at the next 4k-aligned address */
+    page_table     = (unsigned long*) 0X9D000;
+
+    unsigned long address = 0;
+
+    /* The following is to map the first 4 MB of memory */
+
+    for (int i = 0; i < ENTRIES_PER_PAGE; i++)
+    {
+        /* The last 3 bits stand for superviser/user, read/write and present/non-present status respectively.
+         * We set the these attribute bits to superviser, read/write and present */
+        page_table[i] = address | 3;
+        address       = address + 4096;
+    }
+
+    /* The page table initialized above is stored in the page directory */
+    page_directory[0] = page_table;
+
+    /* We then set the attribute bits to superviser, read/write and present */
+    page_directory[0] |= 3;
+
+    /* The rest of the entries in the page_directory have to be filled now.
+     * Since the page tables in these entries have not been created yet, we just store zeros here. 
+     * Also, we mark the attribute sets' present status to 0.
+     * Hence we do a bitwise OR by 2
+     */
+    for (int i = 1; i < ENTRIES_PER_PAGE; i++)
+    {
+        page_directory[1] = 0 | 2;
+    }
 
 }
 
@@ -31,6 +63,7 @@ void PageTable::enable_paging()
      * Write 1 to the 31st bit of CR0 register
      * to enable paging
      */
+    paging_enabled = 1;
     write_cr0(read_cr0() | 0x80000000);
 }
 
