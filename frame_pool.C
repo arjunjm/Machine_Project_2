2600c7d28bcc1b2bb018ffc43886666d2b982ae4
arjunjm <arjunjm@gmail.com>
2014-02-07 19:11:08 -0600

    -Removed STL Lists as these cannot be used. -Changed list implementation to array of structures which seem easier but consume a lot of unnecessary space

--- frame_pool.C
+++ frame_pool.C
@@ -1,9 +1,11 @@
 #include "frame_pool.H"
+#include <stdlib.h>
 
 FramePool::FramePool(unsigned long _base_frame_no,
-                    unsigned long _nframes,
-                    unsigned long _info_frame_no)
+                     unsigned long _nframes,
+                     unsigned long _info_frame_no)
 {
+    number_of_free_frames    = 0;
     base_frame_number        = _base_frame_no;
     number_of_frames_managed = _nframes;
     managament_frame_number  = _info_frame_no;
@@ -13,11 +15,15 @@ FramePool::FramePool(unsigned long _base_frame_no,
      * The get_frame routine will allocate frames from this list
      */
    unsigned long frame_address = _base_frame_no * (FramePool::FRAME_SIZE);
-   do
+
+   for (int i = 0; i < number_of_frames_managed; i++)
    {
-       free_frame_list.push_back(frame_address);
-       frame_address = frame_address + FramePool::FRAME_SIZE;
-   }while (frame_address < (_base_frame_no + _nframes * (FramePool::FRAME_SIZE)));
+       free_frame_list[i].frame_address = frame_address;
+       free_frame_list[i].is_accessible = true;
+       free_frame_list[i].is_allocated  = false;
+       frame_address                    = frame_address + FramePool::FRAME_SIZE;
+       number_of_free_frames            += 1;
+   }
 }
 
 unsigned long FramePool::get_frame()
